cb4a02ab3fe8ca4128f09a2319f82fbb08cce319
Brooklynson31 <mikeharp212@gmail.com>
2016-11-18 09:12:49 -0500

    recursive linear search assigment

--- LearningAlgorithms/src/algo/recursion/RecursionApp.java
+++ LearningAlgorithms/src/algo/recursion/RecursionApp.java
@@ -6,7 +6,9 @@ public class RecursionApp {
 		// TODO Auto-generated method stub
 		System.out.println(reduceByOne(9));
 
-		
+		int[] array = new int[] {10,12,34,5,72,22,31,56};
+		int i = 0; //index where recursive calls will start
+		System.out.println(recursiveLinearSearch(array, i, 31));
 	}
 	
 	public static int reduceByOne(int n){
@@ -16,5 +18,17 @@ public class RecursionApp {
 		}
 		return numOfRecursiveCalls;
 	}	
+	
+	//Assignment: Write an algorithm for a Linear Search Using Recursion
+	public static int recursiveLinearSearch(int array[], int i, int value ){
+		if( i > array.length-1){ //out of bounds check
+			return -1;
+		}
+		else if( array[i] == value){
+			return i;}
+		else{
+			return recursiveLinearSearch(array, i+1, value); //will start at first index position and move one over in each iteration
+		}
+	}
 
 }
