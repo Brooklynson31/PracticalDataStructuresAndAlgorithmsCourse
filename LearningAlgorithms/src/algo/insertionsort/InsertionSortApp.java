2e07f03411f461b950731dee94200da6c99a2b04
Brooklynson31 <mikeharp212@gmail.com>
2016-11-29 08:43:10 -0500

    Optimized Insertion Sort Algorithm

--- LearningAlgorithms/src/algo/insertionsort/InsertionSortApp.java
+++ LearningAlgorithms/src/algo/insertionsort/InsertionSortApp.java
@@ -16,31 +16,16 @@ public class InsertionSortApp {
 	}
 
 	public static void insertionSort(int[] array) {
-//		for(int i = 1; i < array.length-1;i++){
-//			int numberToBeSorted = i; 
-//			int sortedIndex = i-1;
-//			if(array[numberToBeSorted] < array[sortedIndex]){
-//				
-//				for(int backwardsInsertion = sortedIndex;backwardsInsertion >= 0;backwardsInsertion--){
-//					if(array[numberToBeSorted] < array[backwardsInsertion]){
-//					int temp = array[backwardsInsertion];
-//					array[backwardsInsertion] = array[numberToBeSorted];
-//					array[numberToBeSorted] = temp;
-//					numberToBeSorted--;
-//					}
-//				}
-//			}
-//			
-//		}
+
 		
-		for(int numberToBeSorted = 1; numberToBeSorted < array.length-1;numberToBeSorted++){
-			int sortedIndex = numberToBeSorted-1;
-			if(array[numberToBeSorted] < array[sortedIndex]){
+		for(int unsortedIndex = 1; unsortedIndex < array.length-1;unsortedIndex++){
+			int sortedIndex = unsortedIndex-1;
+			if(array[unsortedIndex] < array[sortedIndex]){
 					for(int backwardsInsertion = sortedIndex;backwardsInsertion >= 0;backwardsInsertion--){
 					int temp = array[backwardsInsertion];
-					array[backwardsInsertion] = array[numberToBeSorted];
-					array[numberToBeSorted] = temp;
-					numberToBeSorted--;
+					array[backwardsInsertion] = array[unsortedIndex];
+					array[unsortedIndex] = temp;
+					unsortedIndex--;
 					}
 			}
 			
