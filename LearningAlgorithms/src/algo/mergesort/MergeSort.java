e943b32d97f4f0b52d9536a16b6b8f24dec33370
Brooklynson31 <mikeharp212@gmail.com>
2016-11-30 09:35:47 -0500

    completed splitting step of algorithm

--- LearningAlgorithms/src/algo/mergesort/MergeSort.java
+++ LearningAlgorithms/src/algo/mergesort/MergeSort.java
@@ -9,7 +9,18 @@ public class MergeSort {
 	}
 
 	public static void sort(int[] array, int start, int end) {
-		// TODO Auto-generated method stub
+		if(start < end){ // recursive base condition --> base condition is when all values in array are placed into individual array
+			
+			int mid = (start + end) / 2; 
+			sort(array, start,mid); //will start spliting array left of mid point
+			sort(array,mid+1,end); //will start splitting array right of mid point
+			merge(array, start, mid, end); //once base case if met, will begin to merge 2 sorted arrays
+		}
+		
+	}
+
+	private static void merge(int[] array, int start, int mid, int end) {
+		
 		
 	}
 
