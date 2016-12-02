df8c246d380659e366a573040c1192685bab9ed8
Brooklynson31 <mikeharp212@gmail.com>
2016-12-02 07:51:07 -0500

    update

--- LearningAlgorithms/src/algo/mergesort/MergeSort.java
+++ LearningAlgorithms/src/algo/mergesort/MergeSort.java
@@ -20,6 +20,35 @@ public class MergeSort {
 	}
 
 	private static void merge(int[] array, int start, int mid, int end) {
+		int leftcenter =mid-start +1; // number of indexes on left side of array
+		int rightcenter = end - mid; //  number of indexes on right side of array
+		
+		int[] L = new int[leftcenter+1]; //left side of array with extra spot
+		int[] R = new int[rightcenter+1]; // right side of array with extra spot
+		
+		for(int i = 0; i < leftcenter; i++){
+			L[i] = array[start +i-1]; //populate almost all of  Left Side
+		}
+		
+		for(int j = 0; j < rightcenter; j++){
+			R[j] = array[mid+j]; // populate almost all of Right Side
+		}
+		L[leftcenter+1] = 100000000; //last slot in L
+		R[rightcenter+1] = 100000000; //lsat slot in R
+		
+		
+		int I = 0;
+		int J = 0;
+		for(int k = 0; k < end;k++ ){
+			if(L[I] > R[J]){
+				array[k] = L[I];
+				I = I+1;
+			}
+			else{
+				array[k] = R[J];
+				J = J+1;
+			}
+		}
 		
 		
 	}
