fd00455b20f610b7e4f325daa19d620dfe0d7cf5
Brooklynson31 <mikeharp212@gmail.com>
2017-02-27 08:44:22 -0500

    preorder,depthfirsttraversal

---
+++ LearningDataStructures/src/ads/binarytree/PreOrderDepthFirstTraversal.java
@@ -0,0 +1,39 @@
+package ads.binarytree;
+
+public class PreOrderDepthFirstTraversal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Node<Character> a = new Node<>('A');
+		Node<Character> b = new Node<>('B');
+		Node<Character> c = new Node<>('C');
+		Node<Character> d = new Node<>('D');
+		Node<Character> e = new Node<>('E');
+		Node<Character> f = new Node<>('F');
+		Node<Character> g = new Node<>('G');
+		Node<Character> h = new Node<>('H');
+		
+		a.setLeftChild(b);
+		a.setRightChild(c);
+		c.setLeftChild(d);
+		c.setRightChild(e);
+		d.setLeftChild(f);
+		d.setRightChild(h);
+		e.setRightChild(g);
+		
+		preOrder(a);
+
+
+	}
+	
+	public static void preOrder(Node<Character> root){
+		if(root == null) //base case -- Nothing to Traverse
+			return;
+		
+		System.out.println(root.getData()); //process Node data
+									//Process The Node before recursing to Left and Rigth subtrees
+		preOrder(root.getLeftChild()); 
+		preOrder(root.getRightChild());
+	}
+
+}
