ba0c84a4d37810f149448d6a68591102df993c05
Brooklynson31 <mikeharp212@gmail.com>
2017-03-13 09:27:18 -0400

    LCA

---
+++ LearningDataStructures/src/ads/binarytree/LeastCommonAncestor.java
@@ -0,0 +1,52 @@
+package ads.binarytree;
+
+public class LeastCommonAncestor {
+
+	public static void main(String[] args) {
+		 Node<Integer> a = new Node<>(13);
+	      Node<Integer> b = new Node<>(5);
+	      Node<Integer> c = new Node<>(18);
+	      Node<Integer> d = new Node<>(15);
+	      Node<Integer> e = new Node<>(27);
+	      Node<Integer> f = new Node<>(14);
+	      Node<Integer> g = new Node<>(30);
+	      Node<Integer> h = new Node<>(17);
+	     // Node<Integer> x = new Node<>(3);
+
+		
+		a.setLeftChild(b);
+		a.setRightChild(c);
+		c.setLeftChild(d);
+		c.setRightChild(e);
+		d.setLeftChild(f);
+		d.setRightChild(h);
+		e.setRightChild(g);
+		
+		//System.out.println(lca(a,e,f));
+		lca(a,f,e);
+		
+	}
+	
+	public static Node<Integer> lca(Node<Integer> root,Node<Integer> a,
+			Node<Integer> b){
+		if(root == null) return null; //if we enouncter a null root, no ancestor was found
+		
+		if(root == a || root == b) return root; //if the current is either of the two nodes, then return root itself
+		
+		Node<Integer> leftLCA = lca(root.getLeftChild(),a,b);
+		Node<Integer> rightLCA = lca(root.getRightChild(),a,b); //find the lca for the left and right subtrees
+		
+		if(leftLCA != null && rightLCA != null) //if both exist it means- either the node or its ancestor exists in the left and right subtree
+			return root; 						// so the current node is the LCA
+		
+		if(leftLCA != null){
+			System.out.println(leftLCA.getData());
+			return leftLCA;//if only one of the common ancestors is non null return that
+		}
+		
+		if(rightLCA != null)
+			System.out.println(rightLCA.getData());
+		return rightLCA;
+	}
+
+}
--- LearningDataStructures/src/ads/binarytree/PrintPathsToLeafNode.java
+++ LearningDataStructures/src/ads/binarytree/PrintPathsToLeafNode.java
@@ -24,23 +24,40 @@ public class PrintPathsToLeafNode {
 		e.setRightChild(g);
 		
 		List<Character> test = new ArrayList<Character>();
+		List<Character> test2 = new ArrayList<Character>();
+
 		printPaths(a, test);
+		printPaths2(a,test2);
 		
 	}
-	
+	//Personal Solution
 	public static void printPaths(Node<Character> head, List<Character> test) {
 		if(head == null) return;
 		
-		test.add(head.getData());
-
-		if(head.getLeftChild() == null && head.getRightChild() == null)
+			if(head.getLeftChild() == null && head.getRightChild() == null)
 			System.out.println(test);
 		
+			test.add(head.getData());
 			printPaths(head.getLeftChild(), test);
 			printPaths(head.getRightChild(), test);
 			
-		test.remove(head);
-	
+			
+			
+		test.remove(head.getData());
+	}
+	//Instructor Solution
+	public static void printPaths2(Node<Character> root, List<Character> pathList){//a list keeping track of the curren tpath to this node
+		if(root == null) return; //a null root nothing to do
+		
+		pathList.add(root.getData()); //add the current node to the path and recurse to the left and right child
+		printPaths2(root.getLeftChild(), pathList);
+		printPaths2(root.getRightChild(), pathList);
+		
+		if(root.getLeftChild() == null && root.getRightChild() == null)
+			System.out.println(pathList); //if this is a lead node, print the curren tpath, which has all the nodes leading to this leaf node
+		
+		pathList.remove(root.getData());
+			
 	}
 
 }
